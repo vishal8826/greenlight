4f866ee9c39ee4c9b9aa58176604922c6c7f51de
vichauh2 <vichauh2@cisco.com>
2024-11-01 19:35:28 +0530

    Formatted the code

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/vishal8826/greenlight/internal/data"
 )
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +14,24 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "This is the movie number %d\n", id)
+	data := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Oppenheimer",
+		Year:      2023,
+		Runtime:   230,
+		Genres:    []string{"Sci-fi", "history"},
+		Version:   1,
+	}
+
+	movie := envelope{"movie": data}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
